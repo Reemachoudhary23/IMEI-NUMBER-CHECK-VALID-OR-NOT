92ff22ee31af38c9815fd6d8a80fffe829de17bc
Rishu Tiwari <rishutiwari15062003@gmail.com>
2022-11-24 12:35:09 +0530

    Add files via upload

---
+++ valid IMEI no.c
@@ -0,0 +1,43 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int n;
+printf("enter the n");
+scanf("%d",&n);
+int sum=0;
+int n1=n;
+int m=0;
+while(n1>0){
+    m++;
+    n1=n1/10;
+
+if(m==15){
+        while(m>=1){
+            int d=n%10;
+            if(m%2==0){
+                d=d*2;
+            }
+            while(d>0){
+                int d1=d%10;
+                sum=sum+d1;
+                d=d/10;
+            }
+            n=n/10;
+            m--;
+        }
+
+}
+}
+
+if(sum%10==0){
+    printf("valid IMEI no");
+}
+else if {
+    printf("not valid IMEI no");
+}
+else{
+    printf("invalid no")
+}
+getch();
+}
